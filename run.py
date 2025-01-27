fdd5a21ea45f62d403bcedf5b28f957a6b1e6f6f
aiml4 <36216320-aiml4@users.noreply.replit.com>
2025-01-27 19:29:04 +0000

    User checkpoint: Disable debug mode in Flask app.

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ print(f"VERIFY_TOKEN: {app.config['VERIFY_TOKEN']}")
 if __name__ == "__main__":
     logging.info("Flask app started")
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
